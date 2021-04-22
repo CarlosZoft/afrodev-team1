41e0f7249a3da550b2aad00a25dd11b2eb2da17b
Carlos <carlosoftwar@gmail.com>
2021-04-22 03:01:25 -0300

    Add : Adicionado rotas familia

--- adapters/api/routes/familia.js
+++ adapters/api/routes/familia.js
@@ -1,5 +1,7 @@
 const controller = require('../../controllers/familia');
 const validators = require('../validators/familia');
+const CamposVerifica = require('../validators/CamposVerifica');
+const verify = new CamposVerifica();
 
 const invalidRequestReply = (request, reply, errors) => reply.status(400).json({
   method: request.method,
@@ -9,7 +11,7 @@ const invalidRequestReply = (request, reply, errors) => reply.status(400).json({
 
 module.exports = (app) => {
   app.post('/familia', validators.registerValidator(), async (request, reply) => {
-    /*  #swagger.parameters['post ong object'] = {
+    /*  #swagger.parameters['post family object'] = {
             in: 'body',
             description: "New family values",
             schema: {
@@ -21,64 +23,88 @@ module.exports = (app) => {
                 "$status": active
             }
     } */
-    const errors = validators.validateRequest(request);
-    if (errors.length > 0) {
-      return invalidRequestReply(request, reply, errors);
+    try{
+      const errors = validators.validateRequest(request);
+      if (errors.length) {
+        return invalidRequestReply(request, reply, errors);
+      }
+      const response = await controller.post(request, reply);
+      return reply.json(verify.createFields(response));
     }
-    const response = await controller.post(request, reply);
-    return reply.json(response);
+    catch(error){
+      throw error;
+    }
   });
-}
-//   app.get('/ong', async (request, reply) => {
-//     const response = await controller.get(request, reply);
-//     return reply.json(response);
-//   });
 
-//   app.get('/ong/:id', async (request, reply) => {
-//     const response = await controller.getById(request.params.id, request, reply);
-//     return reply.json(response);
-//   });
+  app.get('/familia', async (request, reply) => {
 
-//   app.put('/ong/:id', validators.updateValidator(), async (request, reply) => {
-//     /*  #swagger.parameters['put ong object'] = {
-//             in: 'body',
-//             description: "New ong values",
-//             schema: {
-//                 "$name": "new ong",
-//                 "$description": "ong description",
-//                 "$email": "aaa@aaa.com",
-//                 "$phone": "(19) 99999-9999"
-//             }
-//     } */
-//     const errors = validators.validateRequest(request);
-//     if (errors.length > 0) {
-//       return invalidRequestReply(request, reply, errors);
-//     }
-//     const response = await controller.put(request.params.id, request, reply);
-//     return reply.json(response);
-//   });
+    try{
+      const response = await controller.get(request, reply);
+      return reply.json(verify.allFields(response));
+    }
+    catch(error){
+      throw error;
+    }
 
-//   app.patch('/ong/:id', validators.patchValidator(), async (request, reply) => {
-//     /*  #swagger.parameters['patch ong object'] = {
-//             in: 'body',
-//             description: "New ong values",
-//             schema: {
-//                 "$name": "new ong",
-//                 "$description": "ong description",
-//                 "$email": "aaa@aaa.com",
-//                 "$phone": "(19) 99999-9999"
-//             }
-//     } */
-//     const errors = validators.validateRequest(request);
-//     if (errors.length > 0) {
-//       return invalidRequestReply(request, reply, errors);
-//     }
-//     const response = await controller.patch(request.params.id, request, reply);
-//     return reply.json(response);
-//   });
+  });
+ 
+  app.get('/familia/:id', async (request, reply) => {
+    
+    try{
 
-//   app.delete('/ong/:id', async (request, reply) => {
-//     const response = await controller.delete(request.params.id, request, reply);
-//     return reply.json(response);
-//   });
-// };
+      const response = await controller.getById(request.params.id, request, reply);
+      return reply.json(verify.createFields(response));
+
+    }
+    catch(error){
+      throw error;
+    }
+
+  });
+
+  app.put('/familia/:id', validators.updateValidator(), async (request, reply) => {
+     /*  #swagger.parameters['put family object'] = {
+            in: 'body',
+            description: "New family values",
+            schema: {
+                "$endereco": "family address",
+                "$quantidadeMembros": 5,
+                "$preferencial": true,
+                "$renda": 1000,
+            }
+      } */
+    try {
+      const errors = validators.validateRequest(request);
+      if (errors.length > 0) {
+        return invalidRequestReply(request, reply, errors);
+      }
+      const response = await controller.put(request.params.id, request, reply);
+      return reply.json(verify.createFields(response));
+    }
+    catch(error){
+      throw error;
+    }
+  });
+  app.patch('/familia/:id', validators.patchValidator(), async (request, reply) => {
+      /*  #swagger.parameters['post family object'] = {
+            in: 'body',
+            description: "New family values",
+            schema: {
+                "$status": "active"
+            }
+      } */
+    try{
+      const errors = validators.validateRequest(request);
+      if (errors.length > 0) {
+        return invalidRequestReply(request, reply, errors);
+      }
+      const response = await controller.patch(request.params.id, request, reply);
+      return reply.json(response);
+    }
+    catch(error){
+      throw error;
+    }
+    
+  });
+
+};
