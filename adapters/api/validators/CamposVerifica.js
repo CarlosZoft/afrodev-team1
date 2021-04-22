91f8961b5aa1c880e33f0d6b9d401b26a6e23b61
Carlos <carlosoftwar@gmail.com>
2021-04-22 03:01:50 -0300

    Add : class para restrição

---
+++ adapters/api/validators/CamposVerifica.js
@@ -0,0 +1,41 @@
+class CamposVerifica {
+   
+    updateFields(body) {
+        var camposPermitidos = ['endereco', 'quantidade_membros', 'preferencial', 'renda'];
+        var bodyFinal = {};
+        camposPermitidos.forEach((campo) => {
+            bodyFinal[campo] = body[campo]        
+        });
+        return bodyFinal;
+    }
+
+    patchFields(body) {
+            var bodyFinal = {}
+            bodyFinal['status'] = body['status'];
+            console.log(bodyFinal);
+            return bodyFinal;
+    }
+    
+    createFields(body) {
+            const camposPermitidos = ['nome_familia', 'endereco', 'quantidade_membros', 'preferencial', 'renda', 'status'];
+            var bodyFinal = {}
+            camposPermitidos.forEach(campo => {
+
+                bodyFinal[campo] = body[campo]
+            })
+            return bodyFinal;
+    }
+    
+    allFields(body) {
+        let dadosFiltrados = this.createFields(body)
+            
+        if(Array.isArray(body)){
+            dadosFiltrados = body.map((dado) => {
+                    return this.createFields(dado);
+            })
+        }
+        return dadosFiltrados;
+    }
+}
+
+module.exports = CamposVerifica;
