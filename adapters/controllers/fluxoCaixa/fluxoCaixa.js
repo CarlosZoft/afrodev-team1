a30369d49f2c6c85b0415579dd120ccbd561a5b9
Eduarda Almeida <eduardapalm27@gmail.com>
2021-04-24 11:25:40 -0300

    update : criando services e Controllers

---
+++ adapters/controllers/fluxoCaixa/fluxoCaixa.js
@@ -0,0 +1,9 @@
+const servicosFluxoCaixa = require('../../../application/services/familia/familia');
+
+exports.post = async (request) => servicosFluxoCaixa.create(request.body);
+
+exports.get = async (request) => servicosFluxoCaixa.findAll(request.body);
+
+exports.getById = async (id) => servicosFluxoCaixa.findById(id);
+
+exports.put = async (id, request) => servicosFluxoCaixa.update(id, request.body);
