390fdff29e40eb9bacd26b2e9cdc82dd60154fea
Eduarda Almeida <eduardapalm27@gmail.com>
2021-04-24 10:34:39 -0300

    create: criando branch

--- config/database.config.js
+++ config/database.config.js
@@ -1,9 +1,9 @@
 require('./environment');
 
 const config = {
-  username: process.env.MYSQL_USERNAME || 'admin',
-  password: process.env.MYSQL_PASSWORD || 'admin',
-  database: process.env.MYSQL_DATABASE || 'mysqldb',
+  username: process.env.MYSQL_USERNAME || 'root',
+  password: process.env.MYSQL_PASSWORD || '@ricduda01',
+  database: process.env.MYSQL_DATABASE || 'ong',
   host: process.env.MYSQL_HOST || 'localhost',
   port: process.env.MYSQL_PORT || '3306',
   dialect: process.env.MYSQL_DIALECT || 'mysql',
