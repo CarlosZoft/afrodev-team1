e8cc32b8bf048e99f660f6788986bbd735a20236
Eduarda Almeida <eduardapalm27@gmail.com>
2021-04-23 22:09:58 -0300

    Update: atualizando o nome da migration

---
+++ database/migrations/20210424005853-familias.js
@@ -0,0 +1,44 @@
+module.exports = {
+  up: async (queryInterface, Sequelize) => queryInterface.createTable('familias', {
+    id: {
+      type: Sequelize.INTEGER,
+      primaryKey: true,
+      allowNull: false,
+      autoIncrement: true,
+    },
+    nome_familia: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+    endereco: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+    quantidade_membros: {
+      type: Sequelize.INTEGER,
+      allowNull: false,
+    },
+    preferencial: {
+      type: Sequelize.BOOLEAN,
+      allowNull: false,
+    },
+    renda: {
+      type: Sequelize.REAL,
+      allowNull: false,
+    },
+    status: {
+      type: Sequelize.ENUM('Ativo', 'Inativo'),
+      allowNull: false,
+    },
+    updated_at: {
+      type: 'timestamp',
+      default: 'now()',
+    },
+    created_at: {
+      type: 'timestamp',
+      default: 'now()',
+    },
+  }),
+
+  down: async (queryInterface) => queryInterface.dropTable('ongs'),
+};
